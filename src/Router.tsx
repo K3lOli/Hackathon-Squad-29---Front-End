803f0fa61e03a335a500aff6936c410426cb534c
Kelson Victor <98909425+K3lOli@users.noreply.github.com>
2024-01-24 18:31:51 -0300

    bug da tela de login arrumado (o bug está descrito no commit anterior)

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,15 +3,11 @@ import { Login } from "./pages/Login";
 import { Cadastro } from "./pages/Cadastro";
 import { Home } from "./pages/Home";
 import { Descobrir } from "./pages/Descobrir";
-import { Header } from "./components/Header";
 export function Router() {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Login />}></Route>
-            </Routes>
-            <Header />
-            <Routes>
                 <Route path="/cadastro" element={<Cadastro />}></Route>
                 <Route path="/home" element={<Home />}></Route>
                 <Route path="/descobrir" element={<Descobrir />}></Route>
