a9a1541bb0897787b9113cf095d299a592d79a5b
Giovani Barbosa <xbarbosagiovani@gmail.com>
2024-01-24 15:53:42 -0300

    Criação e estilização da página de cadastre-se, incluindo página responsiva

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,3 +1,42 @@
+import "./styles.css";
+import "../../index.css";
+import "../Login/styles.css";
+import { CustomInput } from "../../components/Input/index";
+import imgRegister from "../../../public/imagem-pagina-registro.png";
+import { ButtonWithContainer } from "../../components/Buttons/ButtonWithContainer";
+
 export function Cadastro() {
-    return <p>Cadastro</p>;
+    return (
+        <div className="container--register">
+            <div className="imgRegister">
+                <img src={imgRegister} alt="Imagem da página de Registro" />
+            </div>
+            <div className="formContainer">
+                <h3 className="register-title">Cadastre-se</h3>
+                <form>
+                    <div className="container--nome-sobrenome">
+                        <CustomInput labelName="Nome">
+                            <input type="text" />
+                        </CustomInput>
+                        <CustomInput>
+                            <input type="text" placeholder="Sobrenome *" />
+                        </CustomInput>
+                    </div>
+                    <div className="inputContainer">
+                        <CustomInput labelName="Email Address">
+                            <input type="email" id="email" name="email" />
+                        </CustomInput>
+                        <CustomInput labelName="Password">
+                            <input type="password" />
+                        </CustomInput>
+                        <div className="buttonsContainer">
+                            <ButtonWithContainer largura={"100%"}>
+                                Cadastre-se
+                            </ButtonWithContainer>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
