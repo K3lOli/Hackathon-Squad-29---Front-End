ded60d1a9dd5b35c84b697096e886dc99e6bb122
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-01-25 12:41:46 -0300

    conexao com backend implementada e salvamento exato de dependencias

---
+++ src/api/index.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "http://localhost:8000/api",
+    timeout: 10000,
+    headers: { "Content-Type": "application/json" },
+});
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,8 +6,37 @@ import { GoogleButton } from "../../components/Buttons/GoogleButton";
 import { CustomInput } from "../../components/Input";
 import imglogin from "../../assets/img_login.png";
 import { Link } from "react-router-dom";
+import { FormEvent, useState } from "react";
+import api from "../../api";
+import { setItem } from "../../utils/storage";
 
 export function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleLogin(e: FormEvent) {
+        e.preventDefault();
+        try {
+            if (!email || !password) {
+                return;
+            }
+            const response = await api.post("/usuarios/login", {
+                email,
+                senha_hash: password,
+            });
+            const { id, nome, token } = response.data;
+            setItem("token", token);
+            setItem("userId", id.toString());
+            setItem("userName", nome);
+        } catch (error: unknown) {
+            if (error) {
+                console.log(error);
+            } else {
+                alert("Error desconhecido");
+            }
+        }
+    }
+
     return (
         <div className="container">
             <div className="imgLogin">
@@ -15,26 +44,38 @@ export function Login() {
             </div>
             <div className="formContainer">
                 <h3>Entre no Orange Portifólio</h3>
-                <GoogleButton>Entrar com Google</GoogleButton>
-                <form>
+
+                <GoogleButton> Entrar com Google </GoogleButton>
+
+                <form onSubmit={handleLogin}>
                     <h5>Faça login com email</h5>
                     <div className="inputContainer">
                         <CustomInput labelName={"Email Adress"}>
-                            <input type="email" id="email" name="email" />
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </CustomInput>
                         <CustomInput labelName={"Password"}>
-                            <input type="password" />
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </CustomInput>
                     </div>
                     <div className="buttonsContainer">
-                        <Link to="/home" className="link">
-                            <ButtonWithContainerOrange
-                                largura={"100%"}
-                                color={"#fff"}
-                            >
-                                Entrar
-                            </ButtonWithContainerOrange>
-                        </Link>
+                        {/* <Link to="/home" className="link"> */}
+                        <ButtonWithContainerOrange
+                            largura={"100%"}
+                            color={"#fff"}
+                        >
+                            Entrar
+                        </ButtonWithContainerOrange>
+                        {/* </Link> */}
                         <Link to="/cadastro">
                             <ButtonWithoutContainer>
                                 Cadastre-se
