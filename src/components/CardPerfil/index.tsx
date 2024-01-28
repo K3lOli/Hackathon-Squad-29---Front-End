6ea27c6e440c9c4baacdd1daab6ad78e82596853
Giovani Barbosa <xbarbosagiovani@gmail.com>
2024-01-27 22:56:22 -0300

    Instalação da biblioteca e configuração do modal

--- src/components/CardPerfil/index.tsx
+++ src/components/CardPerfil/index.tsx
@@ -1,11 +1,35 @@
 import { ButtonWithContainerGray } from "../Buttons/ButtonWithContainer/GrayButton";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/index";
+import Modal from "react-modal";
+
 import "./styles.css";
+import { useState } from "react";
+const customStyles = {
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+    },
+};
 
 export function CardPerfil() {
     const nome = useSelector((state: RootState) => state.login[0].nome);
     const img = useSelector((state: RootState) => state.login[0].img);
+
+    const [modalIsOpen, setIsOpen] = useState<boolean>(false);
+
+    function abrirModal() {
+        setIsOpen(true);
+    }
+
+    function fecharModal() {
+        setIsOpen(false);
+    }
+
     return (
         <div className="cardContainer">
             <img src={`${img}`} alt="Foto de Perfil" />
@@ -14,12 +38,22 @@ export function CardPerfil() {
                 <h5>{nome}</h5>
                 <p className="subtitle-1">Brasil</p>
                 <ButtonWithContainerGray
+                    onClick={abrirModal}
                     largura="203px"
                     color="rgba(0, 0, 0, 0.38)"
                 >
                     ADICIONAR PROJETO
                 </ButtonWithContainerGray>
             </div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={fecharModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <button onClick={fecharModal}>close</button>
+                <div>I am a modal</div>
+            </Modal>
         </div>
     );
 }
