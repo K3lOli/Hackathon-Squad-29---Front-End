708ad10332dbf98d83cd0ae43c2eea3faeeb1f01
Thiago Terra <thiagoterradev@gmail.com>
2024-01-27 21:30:23 -0300

    Implementação da navegação no header mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import logoOrange from "../../../public/logo-orange-portfolio.svg";
 import notificacao from "../../../public/botao-notificacao.svg";
 import { NavLink } from "react-router-dom";
@@ -10,15 +11,34 @@ import "./styles.css";
 
 export function Header() {
     const img = useSelector((state: RootState) => state.login[0].img);
+
+    const [menuMobileOpen, setMenuMobileOpen] = React.useState(false);
+
+    const toggleMenuMobile = () => {
+        setMenuMobileOpen(!menuMobileOpen);
+    };
+
     return (
         <header className="header">
             <div className="header--content">
+                <div
+                    className="headerMobileNav"
+                    style={{ display: menuMobileOpen ? "block" : "none" }}
+                >
+                    <NavLink to="/meuportfolio">
+                        <li>Meus projetos</li>
+                    </NavLink>
+                    <NavLink to="/descobrir">
+                        <li>Descobrir</li>
+                    </NavLink>
+                </div>
                 <div className="menu-esq-content">
                     <div>
                         <img
                             src={MenuMobile}
                             alt="Menu Mobile"
                             className="menuMobile"
+                            onClick={toggleMenuMobile}
                         />
                         <img
                             src={logoOrange}
