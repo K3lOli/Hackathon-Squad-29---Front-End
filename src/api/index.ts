87ecb30af158d0828a579f14e6480c1c1bd52c79
Kelson Victor <98909425+K3lOli@users.noreply.github.com>
2024-02-03 15:54:28 -0300

    debugando get do criar projetos

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://fcamaradeploy-api.onrender.com/api",
+    // baseURL: "https://fcamaradeploy-api.onrender.com/api",
+    baseURL: "http://localhost:8000/api",
     timeout: 20000,
     headers: { "Content-Type": "application/json" },
 });
--- src/components/CardPerfil/index.tsx
+++ src/components/CardPerfil/index.tsx
@@ -8,12 +8,25 @@ import { CustomInput } from "../Input";
 import { ButtonWithContainerOrange } from "../Buttons/ButtonWithContainer/OrangeButton";
 import Modal from "react-modal";
 import iconeImagem from "../../../public/icone-inserir-imagem.svg";
+import { useForm } from "react-hook-form";
+import api from "../../api/index";
+import { getItem } from "../../utils/storage";
+
+interface FormData {
+    titulo: string;
+    tags: string;
+    link: string;
+    descricao: string;
+    imagem_url?: string;
+}
 
 export function CardPerfil() {
     const nome = useSelector((state: RootState) => state.login[0].nome);
     const img = useSelector((state: RootState) => state.login[0].img);
 
     const [modalIsOpen, setIsOpen] = useState<boolean>(false);
+    const token = getItem("token");
+    console.log(token);
 
     function abrirModal() {
         setIsOpen(true);
@@ -22,6 +35,33 @@ export function CardPerfil() {
     function fecharModal() {
         setIsOpen(false);
     }
+
+    const {
+        register,
+        handleSubmit,
+        // setError,
+        // formState: { errors },
+    } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        // setError("email", {
+        //     type: "custom",
+        //     message: "Email ou senha incorretos",
+        // });
+        // console.log(errors.email?.message);
+        console.log(data);
+        api.post("projetos/cadastrar", {
+            headers: {
+                authorization: `Bearer ${token}`,
+            },
+        })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
     return (
         <div className="cardContainer">
@@ -67,6 +107,7 @@ export function CardPerfil() {
                                         id="input-imagem"
                                         className="input-imagem"
                                         type="file"
+                                        {...register("imagem_url")}
                                     />
                                     <img
                                         className="icone-imagem"
@@ -82,12 +123,17 @@ export function CardPerfil() {
                         </div>
                     </div>
                     <div className="content-dir">
-                        <form className="formulario-projeto" action="">
+                        <form
+                            className="formulario-projeto"
+                            action=""
+                            onSubmit={handleSubmit(onSubmit)}
+                        >
                             <CustomInput>
                                 <input
                                     className="input-form"
                                     type="text"
                                     placeholder="Título"
+                                    {...register("titulo", { required: true })}
                                 />
                             </CustomInput>
                             <CustomInput>
@@ -95,6 +141,7 @@ export function CardPerfil() {
                                     className="input-form"
                                     type="text"
                                     placeholder="Tags"
+                                    {...register("tags", { required: true })}
                                 />
                             </CustomInput>
                             <CustomInput>
@@ -102,14 +149,25 @@ export function CardPerfil() {
                                     className="input-form"
                                     type="text"
                                     placeholder="Link"
+                                    {...register("link")}
                                 />
                             </CustomInput>
                             <CustomInput>
                                 <textarea
                                     className="input-form description"
                                     placeholder="Descrição"
+                                    {...register("descricao", {
+                                        required: true,
+                                    })}
                                 />
                             </CustomInput>
+                            <ButtonWithContainerOrange
+                                largura={"18%"}
+                                color={"#fff"}
+                                type="submit"
+                            >
+                                Salvar
+                            </ButtonWithContainerOrange>
                         </form>
                     </div>
                 </div>
@@ -118,12 +176,6 @@ export function CardPerfil() {
                         Visualizar publicação
                     </p>
                     <div className="salvar-e-cancelar-btn">
-                        <ButtonWithContainerOrange
-                            largura={"18%"}
-                            color={"#fff"}
-                        >
-                            Salvar
-                        </ButtonWithContainerOrange>
                         <ButtonWithContainerGray
                             largura={"18%"}
                             color={"#00000061"}
