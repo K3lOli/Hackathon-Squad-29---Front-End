ab4d34e4ce65d34d3f2a458b9d43f43b08c804c0
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-02-02 21:05:19 -0300

    implementacao de /api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://fcamaradeploy-api.onrender.com",
+    baseURL: "https://fcamaradeploy-api.onrender.com/api",
     timeout: 20000,
     headers: { "Content-Type": "application/json" },
 });
